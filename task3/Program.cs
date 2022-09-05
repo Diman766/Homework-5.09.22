e1ebbb6417903440b52e9500efc4197f20ed3f76
unknown <Dmitiy_yar@mail.ru>
2022-09-05 13:01:05 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 6 || day == 7) Console.WriteLine("Выходной");
+else Console.WriteLine("Будни");
