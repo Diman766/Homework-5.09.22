cebe5c6484a91fe0f298cac7a431761136d2343d
unknown <Dmitiy_yar@mail.ru>
2022-09-05 12:05:39 +0300

    task2

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,3 +1,10 @@
-﻿Console.WriteLine("Введите 3-х значное число");
-string Arr = Console.ReadLine();
-System.Console.WriteLine($"Вторая цифра {Arr[1]}");
+﻿//Console.WriteLine("Введите 3-х значное число");
+//string Arr = Console.ReadLine();
+//Console.WriteLine($"Вторая цифра {Arr[1]}");
+
+Console.WriteLine("Введите 3-х значное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int first = number/10;
+number=first%10;
+
+Console.WriteLine(number);
---
+++ task2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите 3-х значное число");
+string Arr   = Console.ReadLine();
+int size = Convert.ToInt32(Arr.Length);
+if (size>=3) Console.WriteLine(Arr[2]);
+else Console.WriteLine("Число меньше 3-х символов !!!");
+
